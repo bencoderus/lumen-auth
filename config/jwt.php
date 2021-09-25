f43166ac0c1e15ee724a22f207972f65b0d6dbc3
Benjamin Iduwe <bencoderus@gmail.com>
2021-09-25 10:36:42 +0100

    Set JWT token expiration date

---
+++ config/jwt.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| JWT time to live
+|--------------------------------------------------------------------------
+|
+| Specify the length of time (in minutes) that the token will be valid for.
+| Defaults to 1 hour
+|
+*/
+return [
+    'ttl' => 3600,
+];
