4e08b31d97313d8375ce274e1132372ffe7f8272
Benjamin Iduwe <bencoderus@gmail.com>
2022-03-05 18:44:03 +0100

    Convert to post

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,5 @@ $router->group(['middleware' => 'auth'], function ($router) {
     $router->get('/user', 'UserController@profile');
 });
 
-$router->get('/dummy-data', 'DummyController@getDummyData');
+$router->post('/dummy-data', 'DummyController@getDummyData');
 $router->post('/dummy-data/refresh', 'DummyController@refresh');
